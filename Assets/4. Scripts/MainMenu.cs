b6ca0fc10cd60e7a8ac1d2256b9e9edfb0db46e2
devpeeples <111827570+devpeeples@users.noreply.github.com>
2023-02-05 14:40:29 -0500

    9th Art Commit
    Added clouds and bushes bg elements. Also implemented a main menu and different scenes for credits and controls.

---
+++ Assets/4. Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Stage 1");
+    }
+
+    public void SeeCreditsMenu()
+    {
+        SceneManager.LoadScene("Credits Menu");
+    }
+
+    public void SeeControlMenu()
+    {
+        SceneManager.LoadScene("Control Menu");
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+}
