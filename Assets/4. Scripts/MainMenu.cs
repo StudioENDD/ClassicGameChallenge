ccf4b508a9b845f662ef8bff5a650d98cbca5750
devpeeples <111827570+devpeeples@users.noreply.github.com>
2023-02-05 15:37:12 -0500

    10th Art Commit
    Added Win Screen

--- Assets/4. Scripts/MainMenu.cs
+++ Assets/4. Scripts/MainMenu.cs
@@ -23,6 +23,12 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Main Menu");
     }
+    
+    public void Win()
+    {
+        SceneManager.LoadScene("Win Screen");
+    }
+    
     public void QuitGame()
     {
         Debug.Log("quit");
